f980eaf3b61711266b3c469bd64cfce4e1ab8923
rick0x00 <rick.0x00@gmail.com>
2023-04-18 23:49:58 -0300

    add check files before import on read_cli_args.sh

--- read_cli_args.sh
+++ read_cli_args.sh
@@ -10,8 +10,28 @@
 # Remote repository 2: https://gitlab.com/rick0x00/shell_tools #
 # ============================================================ #
 
-source ./project_info.sh
-source ./help.sh
+# ============================================================ #
+# importing functions
+
+function import_script() {
+    script_name="$1"
+    if [ -e "./${script_name}" ]; then
+        # if file "script_name" exist, import
+        source ./${script_name}
+    else
+        # if file "script_name" don't exist, report
+        echo "File \"${script_name}\" does not exist";
+        exit 1;
+    fi    
+}
+
+# import show_changelog-preview and show_version functions
+import_script "project_info.sh"
+
+# import short_help and full_help functions
+import_script "help.sh"
+
+# ============================================================ #
 
 function read_cli_args() {
     local num_arg_errors=0
