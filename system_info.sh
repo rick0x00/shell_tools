2448b6a8101cbe9d68a53e12067924315dc1f462
rick0x00 <rick.0x00@gmail.com>
2023-07-01 23:35:34 -0300

    config (system_info.sh): update system_usage

--- system_info.sh
+++ system_info.sh
@@ -3,7 +3,7 @@
 # ============================================================ #
 # Tool Created date: 23 fev 2023                               #
 # Tool Created by: Henrique Silva (rick.0x00@gmail.com)        #
-# Tool Name: system_info                                       #
+# Tool Name: system_info                                     #
 # Description: Script for help in get system information       #
 # License: MIT License                                         #
 # Remote repository 1: https://github.com/rick0x00/shell_tools #
@@ -43,37 +43,55 @@ containerized=${containerized:-"unknown"}
 # system information
 system_architecture="$(uname -m)"
 
-cpu_model="$(cat /proc/cpuinfo | grep '^model name' | head -n1 | awk -F ':[[:space:]]' '{print $2}' | sed 's/\@[[:space:]]//')"
+cpu_model=$(cat /proc/cpuinfo | grep '^model name' | head -n1 | awk -F ':[[:space:]]' '{print $2}' | sed 's/\@[[:space:]]//')
 cpu_model=${cpu_model:-"unknown"}
+cpu_stats=$(top -bn1 | head -n 6 | grep 'Cpu(s)' | sed 's/^[^:]*://' | sed 's/[[:space:]][[:space:]]\+//g' )
+cpu_idle=$(echo $cpu_stats | tr ',' '\n' | grep 'id' | sed -e 's/[^[:digit:]]*$//g' | sed -e 's/[[:space:]]//g' | tr -d '\n')
 
-mem_info=$(top -bn1 | head -n 6 | grep '^MiB Mem' | sed "s/^[^:]*://" | sed 's/\(\w\+\)\. /\1,/g' | sed 's/[[:space:]][[:space:]]\+//g')
-mem_total=("$(echo $mem_info | tr ',' '\n' | grep 'total' | sed -e 's/[^[:digit:]]*$//g' | tr -d '\n')" "Mb")
+
+mem_stats=$(top -bn1 | head -n 6 | grep '^MiB Mem' | sed "s/^[^:]*://" | sed 's/[[:space:]][[:space:]]\+//g')
+mem_total=("$(echo $mem_stats | tr ',' '\n' | grep 'total' | sed -e 's/[^[:digit:]]*$//g' | sed -e 's/[[:space:]]//g' | tr -d '\n')" "Mb")
 mem_total=${mem_total:-"unknown"}
-mem_free=("$(echo $mem_info | tr ',' '\n' | grep 'free' | sed -e 's/[^[:digit:]]*$//g' | tr -d '\n')" "Mb")
+mem_free=("$(echo $mem_stats | tr ',' '\n' | grep 'free' | sed -e 's/[^[:digit:]]*$//g' | sed -e 's/[[:space:]]//g' | tr -d '\n')" "Mb")
 mem_free=${mem_free:-"unknown"}
-mem_used=("$(echo $mem_info | tr ',' '\n' | grep 'used' | sed -e 's/[^[:digit:]]*$//g' | tr -d '\n')" "Mb")
+mem_used=("$(echo $mem_stats | tr ',' '\n' | grep 'used' | sed -e 's/[^[:digit:]]*$//g' | sed -e 's/[[:space:]]//g' | tr -d '\n')" "Mb")
 mem_used=${mem_used:-"unknown"}
-mem_cached=("$(echo $mem_info | tr ',' '\n' | grep 'cache' | sed -e 's/[^[:digit:]]*$//g' | tr -d '\n')" "Mb")
+mem_cached=("$(echo $mem_stats | tr ',' '\n' | grep 'cache' | sed -e 's/[^[:digit:]]*$//g' | sed -e 's/[[:space:]]//g' | tr -d '\n')" "Mb")
 mem_cached=${mem_cached:-"unknown"}
 echo -e "Memory Info: \nTotal: ${mem_total[@]} \nFree: ${mem_free[@]} \nUsed: ${mem_used[@]} \nCached: ${mem_cached[@]}"
 
-swap_info=$(top -bn1 | head -n 6 | grep '^MiB Swap' | sed "s/^[^:]*://" | sed 's/\(\w\+\)\. /\1,/g' | sed 's/[[:space:]][[:space:]]\+//g')
-swap_total=("$(echo $swap_info | tr ',' '\n' | grep 'total' | sed -e 's/[^[:digit:]]*$//g' | tr -d '\n')" "Mb")
+swap_stats=$(top -bn1 | head -n 6 | grep '^MiB Swap' | sed "s/^[^:]*://" | sed 's/\(\w\+\)\. /\1,/g' | sed 's/[[:space:]][[:space:]]\+//g')
+swap_total=("$(echo $swap_stats | tr ',' '\n' | grep 'total' | sed -e 's/[^[:digit:]]*$//g' | sed -e 's/[[:space:]]//g'  | tr -d '\n')" "Mb")
 swap_total=${swap_total:-"unknown"}
-swap_free=("$(echo $swap_info | tr ',' '\n' | grep 'free' | sed -e 's/[^[:digit:]]*$//g' | tr -d '\n')" "Mb")
+swap_free=("$(echo $swap_stats | tr ',' '\n' | grep 'free' | sed -e 's/[^[:digit:]]*$//g' | sed -e 's/[[:space:]]//g' | tr -d '\n')" "Mb")
 swap_free=${swap_free:-"unknown"}
-swap_used=("$(echo $swap_info | tr ',' '\n' | grep 'used' | sed -e 's/[^[:digit:]]*$//g' | tr -d '\n')" "Mb")
+swap_used=("$(echo $swap_stats | tr ',' '\n' | grep 'used' | sed -e 's/[^[:digit:]]*$//g' | sed -e 's/[[:space:]]//g' | tr -d '\n')" "Mb")
 swap_used=${swap_used:-"unknown"}
-swap_avail=("$(echo $swap_info | tr ',' '\n' | grep 'avail' | sed -e 's/[^[:digit:]]*$//g' | tr -d '\n')" "Mb")
+swap_avail=("$(echo $swap_stats | tr ',' '\n' | grep 'avail' | sed -e 's/[^[:digit:]]*$//g' | sed -e 's/[[:space:]]//g' | tr -d '\n')" "Mb")
 swap_avail=${swap_avail:-"unknown"}
-echo -e "Memory Info: \nTotal: ${swap_total[@]} \nFree: ${swap_free[@]} \nUsed: ${swap_used[@]} \nAvail: ${swap_avail[@]}"
+#echo -e "Swap Info: \nTotal: ${swap_total[@]} \nFree: ${swap_free[@]} \nUsed: ${swap_used[@]} \nAvail: ${swap_avail[@]}"
+
+disk_stats=$(df -h | grep '/$')
+disk_filesystem=$(echo $disk_stats | awk '{print $1}')
+disk_size=$(echo $disk_stats | awk '{print $2}')
+disk_used=$(echo $disk_stats | awk '{print $3}')
+disk_avail=$(echo $disk_stats | awk '{print $4}')
+disk_use_percent=$(echo $disk_stats | awk '{print $5}')
+disk_mounted_point=$(echo $disk_stats | awk '{print $6}')
+#echo -e "Disk Stats: \nDisk Filesystem: $disk_filesystem \nDisk Mounted point: $disk_mounted_point \nDisk size: $disk_size \nDisk avail: $disk_avail \nDisk used: $disk_used \nDisk Use Percent: $disk_use_percent"
+
+sys_load_stats=$(uptime | grep -o 'load average.*$' | sed 's/^[^:]*://' | sed 's/[[:space:]][[:space:]]\+//g' | tr ',' ' ' )
+sys_load_1min=("$(echo $sys_load_stats  | awk '{print $1}')" "%")
+sys_load_5min=("$(echo $sys_load_stats  | awk '{print $2}')" "%")
+sys_load_15min=("$(echo $sys_load_stats  | awk '{print $3}')" "%")
+#echo -e "Load Average: \nlast 1 minute: $sys_load_1min \nlast 5 minute: $sys_load_5min \nlast 15 minute: $sys_load_15min"
 
 # system usage
-cpu_usage="$(top -bn1 | grep 'Cpu(s)' | awk '{print $2+$4+$6}')"
-mem_usage="$(top -bn1 | grep 'MiB Mem' | awk '{print $8}')"
-swap_usage="$(top -bn1 | grep 'MiB Swap' | awk '{print $7}')"
-disk_usage=""
-load_avarage="$(top -bn1 | grep 'load average:' | awk -F 'load average: ' '{print $2}' | awk -F ', ' '{print $1}')"
+cpu_usage=("$(echo 100 $cpu_idle | awk '{print $1 - $2 }')" "%")
+mem_usage=("$(echo $mem_used $mem_total | awk '{ printf "%.2f", ( $1 / $2 ) * 100 }')" "%")
+swap_usage=("$(echo $swap_used $swap_total | awk '{ printf "%.2f", ( $1 / $2 ) * 100 }')" "%")
+disk_usage="$disk_use_percent"
+load_avarage=$sys_load_1min
 
 # network information
 # primary interface = pif
