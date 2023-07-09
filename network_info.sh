477f0a10cf5c5f04e31fa2c6f9692e5a033de89d
rick0x00 <rick.0x00@gmail.com>
2023-07-09 18:09:59 -0300

    Config (network_info.sh): Adjust variables result

--- network_info.sh
+++ network_info.sh
@@ -33,17 +33,25 @@ function internet_connection_test () {
 
 	echo -n "  INTERNET ACCESS: "
     if [ "$ipv4_internet" == "UNAVAILABLE" ] && [ "$ipv6_internet" == "UNAVAILABLE" ]; then
-		echo "UNAVAILABLE"
+		internet_connection_result[0]="UNAVAILABLE"
 	elif [ "$ipv4_internet" == "AVAILABLE" ] || [ "$ipv6_internet" == "AVAILABLE" ]; then
-		echo -n "AVAILABLE"
-		if [ "$ipv4_internet" == "AVAILABLE" ] && [ "$ipv6_internet" == "AVAILABLE" ]; then
-			echo ", IPv4 AND IPv6"
-		elif [ "$ipv4_internet" == "AVAILABLE" ]; then
-			echo ", IPv4 Only"
-		elif [ "$ipv6_internet" == "AVAILABLE" ]; then
-			echo ", IPv6 Only"
+		internet_connection_result[0]="AVAILABLE"
+		if [ "$ipv4_internet" == "AVAILABLE" ]; then
+			internet_connection_result[1]="IPv4"
+		fi
+		if [ "$ipv6_internet" == "AVAILABLE" ]; then
+			internet_connection_result[2]="IPv6"
 		fi
 	fi
+	echo "${internet_connection_result[0]} ${internet_connection_result[1]} ${internet_connection_result[2]}"
+
+	# excluding unnecessary variables
+	unset ipv4_internet ipv6_internet
+
+	# variable "internet_connection_result" contains resume of this function
+	# ${internet_connection_result[0]} is result, "AVAILABLE" OR "UNAVAILABLE"
+	# ${internet_connection_result[1]} is "IPv4" if available
+	# ${internet_connection_result[2]} is "IPv6" if available
 }
 
 function name_resolution_test () {
@@ -51,10 +59,13 @@ function name_resolution_test () {
 
 	echo -n "  Resolution Name: "
 	if $(dig +short google.com &> /dev/null) || $(dig +short nic.br &> /dev/null) ; then
-		echo "SUCCESS"
+		name_resolution_result="SUCCESS"
 	else
-		echo "ERROR"
+		name_resolution_result="ERROR"
 	fi
+	echo "$name_resolution_result"
+	# variable "name_resolution_result" contains resume of this function
+	# $name_resolution_result is result, "SUCCESS" OR "ERROR"
 }
 
 internet_connection_test
