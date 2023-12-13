d76d636d2d81f38683b6385cf6ef116f10c0377f
rick0x00 <rick.0x00@gmail.com>
2023-12-13 01:27:07 -0300

    Docs (shell_color.sh): Add new simple script

---
+++ shell_color.sh
@@ -0,0 +1,98 @@
+#!/usr/bin/env bash
+
+# ============================================================ #
+# Tool Created date: 12 dez 2023                               #
+# Tool Created by: Henrique Silva (rick.0x00@gmail.com)        #
+# Tool Name: shell_color                                       #
+# Description: basic shell text color/style edit               #
+# License: MIT License                                         #
+# Remote repository 1: https://github.com/rick0x00/shell_tools #
+# Remote repository 2: https://gitlab.com/rick0x00/shell_tools #
+# ============================================================ #
+
+# base: https://en.wikipedia.org/wiki/ANSI_escape_code
+
+### Control Sequence Introducer, or CSI, commands, the ESC(ANSI escape sequences)
+csi_esc="\e["
+csi_esc="\033["
+
+### Foreground Color
+text_fg_color_black="${csi_esc}30m"
+text_fg_color_red="${csi_esc}31m"
+text_fg_color_green="${csi_esc}32m"
+text_fg_color_yellow="${csi_esc}33m"
+text_fg_color_blue="${csi_esc}34m"
+text_fg_color_magenta="${csi_esc}35m"
+text_fg_color_cyan="${csi_esc}36m"
+text_fg_color_bright_gray="${csi_esc}37m"
+text_fg_color_gray="${csi_esc}90m"
+text_fg_color_bright_red="${csi_esc}91m"
+text_fg_color_bright_green="${csi_esc}92m"
+text_fg_color_bright_yellow="${csi_esc}93m"
+text_fg_color_bright_blue="${csi_esc}94m"
+text_fg_color_bright_magenta="${csi_esc}95m"
+text_fg_color_bright_cyan="${csi_esc}96m"
+text_fg_color_white="${csi_esc}97m"
+
+r_value_color="255"
+g_value_color="255"
+b_value_color="255"
+text_fg_color_rgb="${csi_esc}38;2;${r_value_color};${g_value_color};${b_value_color}m" # 24-bit RGB 0-255 values set color
+text_fg_color_24bit="${text_bg_color_rgb}"
+
+byte_value_color="255"
+text_fg_color_8bit="${csi_esc}48;5;${byte_value_color}m" # 8-bit 0-255 values set color
+
+
+### Background Color
+text_bg_color_black="${csi_esc}40m"
+text_bg_color_red="${csi_esc}41m"
+text_bg_color_green="${csi_esc}42m"
+text_bg_color_yellow="${csi_esc}43m"
+text_bg_color_blue="${csi_esc}44m"
+text_bg_color_magenta="${csi_esc}45m"
+text_bg_color_cyan="${csi_esc}46m"
+text_bg_color_gray="${csi_esc}47m"
+text_bg_color_bright_gray="${csi_esc}100m"
+text_bg_color_bright_red="${csi_esc}101m"
+text_bg_color_bright_green="${csi_esc}102m"
+text_bg_color_bright_yellow="${csi_esc}103m"
+text_bg_color_bright_blue="${csi_esc}104m"
+text_bg_color_bright_magenta="${csi_esc}105m"
+text_bg_color_bright_cyan="${csi_esc}106m"
+text_bg_color_white="${csi_esc}107m"
+
+r_value_color="255"
+g_value_color="255"
+b_value_color="255"
+text_bg_color_rgb="${csi_esc}48;2;${r_value_color};${g_value_color};${b_value_color}m" # 24-bit RGB 0-255 values set color
+text_bg_color_24bit="${text_bg_color_rgb}"
+
+byte_value_color="255"
+text_bg_color_8bit="${csi_esc}48;5;${byte_value_color}m" # 8-bit 0-255 values set color
+
+### style
+text_style_reset="${csi_esc}0m"
+text_style_bold="${csi_esc}1m"
+text_style_faint="${csi_esc}2m"
+text_style_italic="${csi_esc}3m"
+text_style_underline="${csi_esc}4m"
+
+text_style_slow_blink="${csi_esc}5m"
+text_style_rapid_blink="${csi_esc}6m"
+
+text_style_on_invert="${csi_esc}7m" # invert color FG with BG
+text_style_on_hide="${csi_esc}8m" # hide text
+text_style_on_strike="${csi_esc}9m" # strike text
+
+text_style_double_underline="${csi_esc}21m"
+
+text_style_off_invert="${csi_esc}27m" # revert invert color FG with BG
+text_style_off_hide="${csi_esc}28m" # revert hide text
+text_style_off_strike="${csi_esc}29m" # revert strike text
+
+
+### message
+text_msg="lorem ipsum dolor sit amet"
+
+echo -e "${text_bg_color_rgb} ${text_msg} ${text_style_reset}"
