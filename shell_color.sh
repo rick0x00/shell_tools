4c8e6f4f537f2153cfeef3366f12b2aae05c261a
rick0x00 <rick.0x00@gmail.com>
2023-12-17 22:17:15 -0300

    config (shell_color.sh): Adjust help

--- shell_color.sh
+++ shell_color.sh
@@ -12,6 +12,119 @@
 
 # base: https://en.wikipedia.org/wiki/ANSI_escape_code
 
+
+
+function set_variables_values() {
+    ### message
+    text_msg="lorem ipsum dolor sit amet"
+
+    ### Control Sequence Introducer, or CSI, commands, the ESC(ANSI escape sequences)
+    csi_esc="\e["
+    csi_esc="\033["
+
+    function set_variables_foreground_color() {
+        ### Foreground Color
+        text_fg_color_black="${csi_esc}30m"
+        text_fg_color_red="${csi_esc}31m"
+        text_fg_color_green="${csi_esc}32m"
+        text_fg_color_yellow="${csi_esc}33m"
+        text_fg_color_blue="${csi_esc}34m"
+        text_fg_color_magenta="${csi_esc}35m"
+        text_fg_color_cyan="${csi_esc}36m"
+        text_fg_color_bright_gray="${csi_esc}37m"
+        text_fg_color_gray="${csi_esc}90m"
+        text_fg_color_bright_red="${csi_esc}91m"
+        text_fg_color_bright_green="${csi_esc}92m"
+        text_fg_color_bright_yellow="${csi_esc}93m"
+        text_fg_color_bright_blue="${csi_esc}94m"
+        text_fg_color_bright_magenta="${csi_esc}95m"
+        text_fg_color_bright_cyan="${csi_esc}96m"
+        text_fg_color_white="${csi_esc}97m"
+
+        r_value_color="255"
+        g_value_color="255"
+        b_value_color="255"
+        text_fg_color_rgb="${csi_esc}38;2;${r_value_color};${g_value_color};${b_value_color}m" # 24-bit RGB 0-255 values set color
+        text_fg_color_24bit="${text_bg_color_rgb}"
+
+        byte_value_color="255"
+        text_fg_color_8bit="${csi_esc}48;5;${byte_value_color}m" # 8-bit 0-255 values set color
+
+        r_value_color="255"
+        g_value_color="255"
+        b_value_color="255"
+        text_style_underline_color_rgb="${csi_esc}58;2;${r_value_color};${g_value_color};${b_value_color}m" # 24-bit RGB 0-255 values set color
+        text_style_underline_color_24bit="${text_style_underline_color_rgb}"
+
+        text_style_off_underline_color="${csi_esc}59m" # turn off underline color
+    }
+    set_variables_foreground_color
+
+    function set_variables_background_color() {
+        ### Background Color
+        text_bg_color_black="${csi_esc}40m"
+        text_bg_color_red="${csi_esc}41m"
+        text_bg_color_green="${csi_esc}42m"
+        text_bg_color_yellow="${csi_esc}43m"
+        text_bg_color_blue="${csi_esc}44m"
+        text_bg_color_magenta="${csi_esc}45m"
+        text_bg_color_cyan="${csi_esc}46m"
+        text_bg_color_gray="${csi_esc}47m"
+        text_bg_color_bright_gray="${csi_esc}100m"
+        text_bg_color_bright_red="${csi_esc}101m"
+        text_bg_color_bright_green="${csi_esc}102m"
+        text_bg_color_bright_yellow="${csi_esc}103m"
+        text_bg_color_bright_blue="${csi_esc}104m"
+        text_bg_color_bright_magenta="${csi_esc}105m"
+        text_bg_color_bright_cyan="${csi_esc}106m"
+        text_bg_color_white="${csi_esc}107m"
+
+        r_value_color="255"
+        g_value_color="0"
+        b_value_color="0"
+        text_bg_color_rgb="${csi_esc}48;2;${r_value_color};${g_value_color};${b_value_color}m" # 24-bit RGB 0-255 values set color
+        text_bg_color_24bit="${text_bg_color_rgb}"
+
+        byte_value_color="255"
+        text_bg_color_8bit="${csi_esc}48;5;${byte_value_color}m" # 8-bit 0-255 values set color
+    }
+    set_variables_background_color
+
+    function set_variables_styles() {
+        ### style
+        text_style_reset="${csi_esc}0m" # All attributes become turned off
+
+        text_style_bold="${csi_esc}1m"
+        text_style_faint="${csi_esc}2m"
+        text_style_italic="${csi_esc}3m"
+        text_style_underline="${csi_esc}4m"
+        text_style_overline="${csi_esc}53m" 
+
+        text_style_slow_blink="${csi_esc}5m"
+        text_style_rapid_blink="${csi_esc}6m"
+
+        text_style_invert="${csi_esc}7m" # invert color FG with BG
+        text_style_hide="${csi_esc}8m" # hide text
+        text_style_strike="${csi_esc}9m" # strike text
+
+        text_style_double_underline="${csi_esc}21m"
+
+        text_style_off_bold="${csi_esc}22m" # turn off bold/faint(enable normal intensity)
+        text_style_off_italic="${csi_esc}23m" # turn off italic
+        text_style_off_underline="${csi_esc}24m" # turn off underline
+        text_style_off_overline="${csi_esc}55m" # turn off overline
+
+        text_style_off_blink="${csi_esc}25m" # turn off blink
+
+        text_style_off_invert="${csi_esc}27m" # turn off invert color FG with BG
+        text_style_off_hide="${csi_esc}28m" # turn off hide text
+        text_style_off_strike="${csi_esc}29m" # turn off strike text
+
+    }
+    set_variables_styles
+}
+set_variables_values
+
 function short_help() {
     echo -e "usage:\n    $(echo $0) [OPTIONS] <OBJECT>"
     echo "execute  \"$(echo $0) --help\" to get more information...";
@@ -19,25 +132,24 @@ function short_help() {
 
 function full_help() {
     echo "";
-    echo -e "\e[1;34mDESCRIPTION: \e[0m";
+    echo -e "${text_style_bold}DESCRIPTION:${text_style_reset}";
     echo "  tool to format text.";
     echo "";
-    echo -e "\e[1;34mUSAGE: \e[0m";
+    echo -e "${text_style_bold}USAGE:${text_style_reset}";
     echo "  $(echo $0) [OPTIONS] <OBJECT> ...";
     echo "";
-    echo -e "\e[1;34mOPTIONS: \e[0m";
+    echo -e "${text_style_bold}OPTIONS:${text_style_reset}";
     echo "  -fgc, --fgc, -foreground_color, --foreground_color <color>"
     echo "      Foreground color.";
-    echo "      [default: white]"
-    echo "      [possible values of <color>: white, black, red, green, yellow, blue, magenta, cyan, gray, bright_gray, bright_red, bright_green, bright_yellow, bright_blue, bright_magenta, bright_cyan, rgb(24-bit RGB 0-255), underline_rgb(24-bit RGB 0-255)";
+    echo -e "      [possible values of <color>: ${text_fg_color_white}white${text_style_reset}, ${text_fg_color_black}black${text_style_reset}(black), ${text_fg_color_red}red${text_style_reset}, ${text_fg_color_green}green${text_style_reset}, ${text_fg_color_yellow}yellow${text_style_reset}, ${text_fg_color_blue}blue${text_style_reset}, ${text_fg_color_magenta}magenta${text_style_reset}, ${text_fg_color_cyan}cyan${text_style_reset}, ${text_fg_color_gray}gray${text_style_reset}, ${text_fg_color_bright_gray}bright_gray${text_style_reset}, ${text_fg_color_bright_red}bright_red${text_style_reset}, ${text_fg_color_bright_green}bright_green${text_style_reset}, ${text_fg_color_bright_yellow}bright_yellow${text_style_reset}, ${text_fg_color_bright_blue}bright_blue${text_style_reset}, ${text_fg_color_bright_magenta}bright_magenta${text_style_reset}, ${text_fg_color_bright_cyan}bright_cyan${text_style_reset}, ${text_fg_color_red}r${text_fg_color_green}g${text_fg_color_blue}b${text_fg_color_white}(24-bit RGB 0-255)${text_style_reset}, underline_${text_style_underline}${csi_esc}58;2;255;0;0mr${csi_esc}58;2;0;255;0mg${csi_esc}58;2;0;0;255mb${text_style_reset}(24-bit RGB 0-255)";  
     echo "  -bgc, --bgc, -background_color, --background_color <color>"
     echo "      Background color.";
     echo "      [default: black]"
-    echo "      [possible values of <color>: white, black, red, green, yellow, blue, magenta, cyan, gray, bright_gray, bright_red, bright_green, bright_yellow, bright_blue, bright_magenta, bright_cyan, rgb(24-bit RGB 0-255), underline_rgb(24-bit RGB 0-255)";
+    echo -e "      [possible values of <color>: ${text_bg_color_white}white${text_style_reset}(white), ${text_bg_color_black}black${text_style_reset}, ${text_bg_color_red}red${text_style_reset}, ${text_bg_color_green}green${text_style_reset}, ${text_bg_color_yellow}yellow${text_style_reset}, ${text_bg_color_blue}blue${text_style_reset}, ${text_bg_color_magenta}magenta${text_style_reset}, ${text_bg_color_cyan}cyan${text_style_reset}, ${text_bg_color_gray}gray${text_style_reset}, ${text_bg_color_bright_gray}bright_gray${text_style_reset}, ${text_bg_color_bright_red}bright_red${text_style_reset}, ${text_bg_color_bright_green}bright_green${text_style_reset}, ${text_bg_color_bright_yellow}bright_yellow${text_style_reset}, ${text_bg_color_bright_blue}bright_blue${text_style_reset}, ${text_bg_color_bright_magenta}bright_magenta${text_style_reset}, ${text_bg_color_bright_cyan}bright_cyan${text_style_reset}, ${text_bg_color_red}r${text_bg_color_green}g${text_bg_color_blue}b${text_style_reset}(24-bit RGB 0-255)";  
     echo "  -es, --es, -enable_style, --enable_style <style>"
     echo "      Enable Style of text.";
     echo "      [default: reset]"
-    echo "      [possible values of <style>: reset, bold, faint, italic, underline, overline, rapid_blink, slow_blink, invert, hide, strike, double_underline";
+    echo -e "      [possible values of <style>: ${text_style_reset}reset${text_style_reset}, ${text_style_bold}bold${text_style_reset}, ${text_style_faint}faint${text_style_reset}, ${text_style_italic}italic${text_style_reset}, ${text_style_underline}underline${text_style_reset}, ${text_style_overline}overline${text_style_reset}, ${text_style_rapid_blink}rapid_blink${text_style_reset}, ${text_style_slow_blink}slow_blink${text_style_reset}, ${text_style_invert}invert${text_style_reset}, ${text_style_hide}hide${text_style_reset}(hide), ${text_style_strike}strike${text_style_reset}, ${text_style_double_underline}double_underline${text_style_reset}";
     #echo "  -ds, --ds, -disable_style, --disable_style <style>"
     #echo "      Disable Style of text.";
     #echo "      [default: bold]"
@@ -51,12 +163,12 @@ function full_help() {
     echo "  -H, --help"
     echo "      Show Full Help message.";
     echo "";
-    echo -e "\e[1;34mEXAMPLES: \e[0m";
+    echo -e "${text_style_bold}EXAMPLES:${text_style_reset}";
     echo "  $(echo $0) --fgc red -bgc white --enable_style rapid_blink -msg \"Hello World!\"";
     echo "";
 }
 
-function conf_text_format() {
+function message_maker() {
     text_format_function_args="$*"
     if [ -z "${text_format_function_args}" ]; then
         echo "Error, no options specified"
@@ -64,117 +176,7 @@ function conf_text_format() {
         exit 0;
     fi
 
-    function set_variables_values() {
-        ### message
-        text_msg="lorem ipsum dolor sit amet"
-
-        ### Control Sequence Introducer, or CSI, commands, the ESC(ANSI escape sequences)
-        csi_esc="\e["
-        csi_esc="\033["
-
-        function set_variables_foreground_color() {
-            ### Foreground Color
-            text_fg_color_black="${csi_esc}30m"
-            text_fg_color_red="${csi_esc}31m"
-            text_fg_color_green="${csi_esc}32m"
-            text_fg_color_yellow="${csi_esc}33m"
-            text_fg_color_blue="${csi_esc}34m"
-            text_fg_color_magenta="${csi_esc}35m"
-            text_fg_color_cyan="${csi_esc}36m"
-            text_fg_color_bright_gray="${csi_esc}37m"
-            text_fg_color_gray="${csi_esc}90m"
-            text_fg_color_bright_red="${csi_esc}91m"
-            text_fg_color_bright_green="${csi_esc}92m"
-            text_fg_color_bright_yellow="${csi_esc}93m"
-            text_fg_color_bright_blue="${csi_esc}94m"
-            text_fg_color_bright_magenta="${csi_esc}95m"
-            text_fg_color_bright_cyan="${csi_esc}96m"
-            text_fg_color_white="${csi_esc}97m"
-
-            r_value_color="255"
-            g_value_color="255"
-            b_value_color="255"
-            text_fg_color_rgb="${csi_esc}38;2;${r_value_color};${g_value_color};${b_value_color}m" # 24-bit RGB 0-255 values set color
-            text_fg_color_24bit="${text_bg_color_rgb}"
-
-            byte_value_color="255"
-            text_fg_color_8bit="${csi_esc}48;5;${byte_value_color}m" # 8-bit 0-255 values set color
-
-            r_value_color="255"
-            g_value_color="255"
-            b_value_color="255"
-            text_style_underline_color_rgb="${csi_esc}58;2;${r_value_color};${g_value_color};${b_value_color}m" # 24-bit RGB 0-255 values set color
-            text_style_underline_color_24bit="${text_style_underline_color_rgb}"
-
-            text_style_off_underline_color="${csi_esc}59m" # turn off underline color
-        }
-        set_variables_foreground_color
-
-        function set_variables_background_color() {
-            ### Background Color
-            text_bg_color_black="${csi_esc}40m"
-            text_bg_color_red="${csi_esc}41m"
-            text_bg_color_green="${csi_esc}42m"
-            text_bg_color_yellow="${csi_esc}43m"
-            text_bg_color_blue="${csi_esc}44m"
-            text_bg_color_magenta="${csi_esc}45m"
-            text_bg_color_cyan="${csi_esc}46m"
-            text_bg_color_gray="${csi_esc}47m"
-            text_bg_color_bright_gray="${csi_esc}100m"
-            text_bg_color_bright_red="${csi_esc}101m"
-            text_bg_color_bright_green="${csi_esc}102m"
-            text_bg_color_bright_yellow="${csi_esc}103m"
-            text_bg_color_bright_blue="${csi_esc}104m"
-            text_bg_color_bright_magenta="${csi_esc}105m"
-            text_bg_color_bright_cyan="${csi_esc}106m"
-            text_bg_color_white="${csi_esc}107m"
-
-            r_value_color="255"
-            g_value_color="0"
-            b_value_color="0"
-            text_bg_color_rgb="${csi_esc}48;2;${r_value_color};${g_value_color};${b_value_color}m" # 24-bit RGB 0-255 values set color
-            text_bg_color_24bit="${text_bg_color_rgb}"
-
-            byte_value_color="255"
-            text_bg_color_8bit="${csi_esc}48;5;${byte_value_color}m" # 8-bit 0-255 values set color
-        }
-        set_variables_background_color
-
-        function set_variables_styles() {
-            ### style
-            text_style_reset="${csi_esc}0m" # All attributes become turned off
-
-            text_style_bold="${csi_esc}1m"
-            text_style_faint="${csi_esc}2m"
-            text_style_italic="${csi_esc}3m"
-            text_style_underline="${csi_esc}4m"
-            text_style_overline="${csi_esc}53m" 
-
-            text_style_slow_blink="${csi_esc}5m"
-            text_style_rapid_blink="${csi_esc}6m"
-
-            text_style_invert="${csi_esc}7m" # invert color FG with BG
-            text_style_hide="${csi_esc}8m" # hide text
-            text_style_strike="${csi_esc}9m" # strike text
-
-            text_style_double_underline="${csi_esc}21m"
-
-            text_style_off_bold="${csi_esc}22m" # turn off bold/faint(enable normal intensity)
-            text_style_off_italic="${csi_esc}23m" # turn off italic
-            text_style_off_underline="${csi_esc}24m" # turn off underline
-            text_style_off_overline="${csi_esc}55m" # turn off overline
-
-            text_style_off_blink="${csi_esc}25m" # turn off blink
-
-            text_style_off_invert="${csi_esc}27m" # turn off invert color FG with BG
-            text_style_off_hide="${csi_esc}28m" # turn off hide text
-            text_style_off_strike="${csi_esc}29m" # turn off strike text
-
-        }
-        set_variables_styles
-    }
-    set_variables_values
-
+    ## read CLI Args
     while [ -n "$1" ]; do
         if [ "$1" == "-h" ]; then
             short_help;
@@ -486,12 +488,15 @@ function conf_text_format() {
         unset message_set
 
     done
+
     if [[ -n "$num_arg_errors" ]]; then
         echo "error: $num_arg_errors invalid arguments"
         exit 1;    
     fi
+
+    # output the maked message
+    echo -e "${maked_message}${text_style_reset}"
+
 }
 
-conf_text_format $*
-
-echo -e "${maked_message}${text_style_reset}"
+message_maker $*
