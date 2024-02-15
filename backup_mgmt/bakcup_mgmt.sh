041c339c8045d16ac71111178513c701cae9b56f
rick0x00 <rick.0x00@gmail.com>
2024-02-14 22:51:39 -0300

    Add basic backup and retentions functions

--- backup_mgmt/bakcup_mgmt.sh
+++ backup_mgmt/bakcup_mgmt.sh
@@ -47,7 +47,6 @@ function backup_mgmt_full_help() {
     echo "";
 }
 
-
 function backup_mgmt_option_setting() {
     #echo "imputed args: $@"
     backup_mgmt_function_args="$@"
@@ -85,7 +84,7 @@ function backup_mgmt_option_setting() {
                             local backup_source_set="$2"
                             TRIGGER_BACKUP_SOURCE_SET="1"
                         fi
-                        #echo "Source set: $backup_source_set"
+                        #echo "source set: $backup_source_set"
                         shift
                     done
                 else
@@ -166,33 +165,130 @@ function backup_mgmt_option_setting() {
 
     # output the defined options
 
-    echo "backup_source_set: $backup_source_set"
+    #echo "backup_source_set: $backup_source_set"
     export BACKUP_SOURCE="${backup_source_set}"
-    echo "backup_destination_set: $backup_destination_set"
+    echo "SOURCE SPECIFIED: $backup_source_set"
+    #echo "backup_destination_set: $backup_destination_set"
     export BACKUP_DESTINATION="${backup_destination_set}"
-    echo "backup_retention_set: $backup_retention_set"
+    echo "DESTINATION SPECIFIED: $backup_destination_set"
+    #echo "backup_retention_set: $backup_retention_set"
     export BACKUP_RETENTION="${backup_retention_set}"
+    echo "RETENTION SPECIFIED: $backup_retention_set"
+
+    # reserving date
+    export BACKUP_DATE=$(date +"Y%Ym%md%d-H%HM%MS%S")
+
+    # reserving hostname
+    export BACKUP_HOST_HOSTNAME="$(hostname)"
 
     return 0
 }
 
+function backup_checks() {
+    echo "## BACKUP CHECKS"
+
+    # make sure you have "/" at the end of the variable
+    if [[ "${BACKUP_DESTINATION: -1}" != "/" ]]; then
+        BACKUP_DESTINATION="${BACKUP_DESTINATION}/"
+    fi
+
+    echo -n "# check if destination directory exists. "
+    # check if destination directory exists
+    if [ ! -d ${BACKUP_DESTINATION} ]; then
+        echo -n "# destination directory not exists! "
+        echo "# creating..."
+        mkdir -p ${BACKUP_DESTINATION}
+    else
+        echo -n "# destination directory already exists "
+        echo "# skipping..."
+    fi
+
+    echo "# check if source specified exists"
+    # loop to grant check multiple source specified
+    for to_check in ${BACKUP_SOURCE}; do
+        echo -n "# check if \"$to_check\" exist:"
+        if [ -e "$to_check" ]; then
+            echo " exists..."
+        else
+            echo " NOT EXIST."
+            exit 1
+        fi 
+    done
+}
+
 function backup_create() {
     echo "## CREATING BACKUP"
+
+    # specify backup file URL
+    local extension="tar.gz"
+    local backup_file="${BACKUP_DESTINATION}${BACKUP_DATE}.${extension}"
+    local log_file="${BACKUP_DESTINATION}${BACKUP_DATE}.log"
+
     function backup_create_file(){
-        echo "# CREATING BACKUP FILE"
+        echo "# CREATING BACKUP FILE <${backup_file}>..."
+        tar -czf "${backup_file}" ${BACKUP_SOURCE}
     }
+
     function backup_create_log(){
-        echo "# CREATING LOG FILE"
+        echo "# CREATING LOG FILE <${log_file}>..."
+        echo "# log of backup file <${backup_file}>" > "${log_file}"
+        echo "BACKUP_DATE=\"${BACKUP_DATE}\"" >> "${log_file}"
+        echo "BACKUP_HOST_HOSTNAME=\"${BACKUP_HOST_HOSTNAME}\"" >> "${log_file}"
+        echo "BACKUP_RETENTION=\"${BACKUP_RETENTION}\"" >> "${log_file}"
+        echo "BACKUP_DESTINATION=\"${BACKUP_DESTINATION}\"" >> "${log_file}"
+        echo "BACKUP_SOURCE=\"${BACKUP_SOURCE}\"" >> "${log_file}"
     }
+
+    backup_create_file
+    
+    # check error of creation backup file
+    if [ $? -ne 0 ]; then
+        echo "# BACKUP ERROR: aborting..."
+        rm -f "${backup_file}"
+        exit 1
+    fi
+
+    backup_create_log
 }
 
 function backup_retention() {
     echo "## SETTING RETENTION OF BACKUPS"
-}
 
+    if [[ ${BACKUP_RETENTION} -ne 0 ]]; then
+        echo "# retention is specified"
+
+        #local existent_backups="$(ls -t /var/backups/*.tar*)"
+        local quantity_of_existent_backups="$(ls -t ${BACKUP_DESTINATION}*.tar* | wc -l)"
+        
+        #echo "Existent backups: ${existent_backups}"
+        echo "# quantity of existent backups: ${quantity_of_existent_backups}"
+
+        # check if existe surplus backups
+        if [[ ${quantity_of_existent_backups} -gt ${BACKUP_RETENTION} ]]; then
+            local quantity_of_surplus_backups_detected="$((${quantity_of_existent_backups} - ${BACKUP_RETENTION}))"
+            echo "# \"${quantity_of_surplus_backups_detected}\" surplus backups detected"
+            local surplus_backups="$(ls -t ${BACKUP_DESTINATION}*.tar* | tail -n ${quantity_of_surplus_backups_detected})"
+            echo "# surplus backups: "
+            echo "${surplus_backups}"
+            for to_delete_backup in ${surplus_backups} ; do
+                local backup_file_to_delete="${to_delete_backup}"
+                local backup_log_to_delete="${backup_file_to_delete%tar.gz}log"
+                echo "# deleting <${backup_file_to_delete}> and <${backup_log_to_delete}>..."
+                rm -f ${backup_file_to_delete}
+                rm -f ${backup_log_to_delete}
+            done
+        fi
+
+    else
+        echo "# retentions not specified."
+    fi
+
+
+}
 
 
 backup_mgmt_option_setting $@
 
+backup_checks
 backup_create
 backup_retention
